32201f985274d26ae4c292eaa4fb715e51df9170
nomis5903 <nomiteksystemsltd@gmail.com>
2023-02-21 16:36:53 -0800

    I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char r;
+
+	for (r = 'a' ; r <= 'z' ; r++)
+		putchar(r);
+	putchar('\n');
+	return (0);
+}
