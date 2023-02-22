e0205f4cb99a82db8d5dc6c6b4b2338f21c0cde0
nomis5903 <nomiteksystemsltd@gmail.com>
2023-02-21 18:13:46 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int r;
+
+	for (r = 0 ; r < 10 ; r++)
+		printf("%d", r);
+	printf("\n");
+	return (0);
+}
