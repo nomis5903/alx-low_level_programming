4e61922bb59b9e2cb2aaeeaa40e44bd4e4339802
nomis5903 <nomiteksystemsltd@gmail.com>
2023-02-21 18:27:05 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int r;
+
+	for (r = 0 ; r < 10 ; r++)
+		putchar(r + '0');
+	putchar('\n');
+	return (0);
+}
