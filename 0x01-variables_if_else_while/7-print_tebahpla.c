ed0bd6587fe2617c9073eacf6dc233163d7aa740
nomis5903 <nomiteksystemsltd@gmail.com>
2023-02-21 18:48:30 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char r;
+
+	for (r = 'z' ; r >= 'a' ; r--)
+		putchar(r);
+	putchar('\n');
+	return (0);
+}
