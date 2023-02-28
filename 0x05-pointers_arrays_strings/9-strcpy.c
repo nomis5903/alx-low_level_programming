feb8e92a905cb8948ba3548d99eabe7bf1962629
nomis5903 <nomiteksystemsltd@gmail.com>
2023-02-27 17:28:25 -0800

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strcpy - copy the string in source to dest
+* @dest: destiny char
+* @src: source char
+* Return: copy char in destiny
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int index;
+
+for (index = 0; src[index] != '\0'; index++)
+dest[index] = src[index];
+dest[index] = '\0';
+return (dest);
+}
