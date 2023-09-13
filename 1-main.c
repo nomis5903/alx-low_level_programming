86613e036293ff5c2909e30108d49dc179badcf9
nomis5903 <nomiteksystemsltd@gmail.com>
2023-09-12 18:51:50 -0700

    1. A dog is the only thing on earth that loves you more than you love yourself

--- 1-main.c
+++ 1-main.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * main - check the code
@@ -8,11 +8,9 @@
  */
 int main(void)
 {
-    char c;
+    struct dog my_dog;
 
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
     return (0);
 }
